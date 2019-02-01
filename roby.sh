027d4172357a09f0a011ef6094f9a851dd4c9c01
Gwendal <didotgwendal@gmail.com>
2019-02-01 12:28:43 +0100

    Installe de logcheck et maj auto de Malilog

--- roby.sh
+++ roby.sh
@@ -5,10 +5,20 @@ if  ! git="$(type -p git)" || [[ -z $git ]]; then
   apt-get install git -y
 fi
 
+# add usefull git alias
+git config --global alias.up '!git remote update -p; git merge --ff-only @{u}'
+
+# Check if logcheck is not installed
+if ! logcheck="$(type -p logcheck)" || [[ -z $logcheck ]]; then
+  apt-get install logcheck -y
+fi
+
 # Clone Malilog if needed
 if [ ! -d "Malilog" ]; then
   git clone https://github.com/Projet-SIEM/Malilog.git
-  ls
 fi
 
-ls
+cd Malilog
+# Update Malilog if needed
+git checkout master && git up;
+cd .. && ls
